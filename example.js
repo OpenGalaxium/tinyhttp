61fd04d6be8ed9ba9a70067443ed80ccaa92baab
cafeed28 <artlekseev@gmail.com>
2021-02-17 21:59:35 +0500

    partur

--- example.js
+++ example.js
@@ -3,7 +3,11 @@ const tinyhttp = require('./index')
 const app = new tinyhttp(1337, 'localhost')
 
 app.get('/', (req, res) => {
-  res.send('welcum')
+	res.send('well cum to get')
+})
+
+app.route('/get', 'get', (req, res) => {
+	res.send('well cum to route(get)')
 })
 
 app.run()
